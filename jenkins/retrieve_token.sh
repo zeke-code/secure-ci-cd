a54daddf1a8ec2baf5ad67581140e610ef72bf00
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-07-31 20:25:34 +0200

    Fix sonarqube credential setting

--- jenkins/retrieve_token.sh
+++ jenkins/retrieve_token.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 
-# Function to wait for SonarQube to be fully operational
-function wait_for_sonarqube() {
-  sleep 80
-}
+# Wait for SonarQube to start
+sleep 80
 
-# Wait for SonarQube
-wait_for_sonarqube
+# Create SonarQube token
+TOKEN=$(curl -u admin:admin -X POST 'http://sonarqube:9000/api/user_tokens/generate' -d "name=jenkins-token" | jq -r .token)
 
-# Function to wait for Jenkins to be fully operational
-function wait_for_jenkins() {
-  echo "Waiting for Jenkins to be fully operational..."
-  until [ "$(curl -s -o /dev/null -w '%{http_code}' http://localhost:8080)" == "200" ]; do
-    echo "Jenkins is not yet ready. Waiting..."
-    sleep 10
-  done
-  echo "Jenkins is operational."
-}
+echo "Generated token: $TOKEN"
 
-# Start Jenkins in the background
-/usr/local/bin/jenkins.sh &
+# Save token to a file (or securely store it in a Docker secret)
+echo $TOKEN > /var/jenkins_home/sonarqube_token.env
 
-# Wait for Jenkins to be fully operational
-wait_for_jenkins
+# Export the token as an environment variable
+export SONARQUBE_TOKEN=$TOKEN
 
-# Fetch the SonarQube authentication token
-response=$(curl -u admin:admin -X POST 'http://sonarqube:9000/api/user_tokens/generate' -d 'name=jenkins_token')
-token=$(echo $response | sed -n 's/.*"token":"\([^"]*\)".*/\1/p')
-echo "Generated Token: $token"
-echo "SONARQUBE_TOKEN=${token}" > /var/jenkins_home/sonarqube_token.env
-
-# Keep the container running
-tail -f /dev/null
+# Start Jenkins
+exec /usr/local/bin/jenkins.sh
