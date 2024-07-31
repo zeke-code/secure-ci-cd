e285f7846e088a1514e65530e194a79f44012cd1
DemaControlsUs <116798879+DemaControlsUs@users.noreply.github.com>
2024-08-01 00:19:58 +0200

    Fix important data consistency in jenkins container and add automatic webhook creation upon first startup

---
+++ jenkins/init.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+FLAG_FILE="/var/jenkins_home/sonarqube_token_generated.flag"
+TOKEN_FILE="/var/jenkins_home/sonarqube_token.env"
+WEBHOOK_NAME="jenkins"
+WEBHOOK_URL="http://jenkins:8080/sonarqube-webhook"
+
+# Check if the token has already been generated
+if [ ! -f "$FLAG_FILE" ]; then
+
+  # Wait for SonarQube container to start
+  sleep 80
+
+  # Create webhook for quality gate through API
+  curl -u admin:admin -X POST 'http://sonarqube:9000/api/webhooks/create' -d "name=$WEBHOOK_NAME&url=$WEBHOOK_URL"
+
+  # Create SonarQube token through API
+  TOKEN=$(curl -u admin:admin -X POST 'http://sonarqube:9000/api/user_tokens/generate' -d "name=jenkins-token" | jq -r .token)
+
+  # Save token to a file
+  echo $TOKEN > $TOKEN_FILE
+
+  # Export the token as an environment variable for Jenkins
+  export SONARQUBE_TOKEN=$TOKEN
+
+  # Create the flag file to indicate that the token has been generated
+  touch "$FLAG_FILE"
+else
+  echo "Token has already been generated. Loading from file."
+  if [ -f "$TOKEN_FILE" ]; then
+    export SONARQUBE_TOKEN=$(cat $TOKEN_FILE)
+  else
+    echo "Error: Token file not found."
+    exit 1
+  fi
+fi
+
+# Start Jenkins
+exec /usr/local/bin/jenkins.sh
--- start.sh
+++ start.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
+command_exists() {
+    command -v "$1" &> /dev/null
+}
+
 # Check if Docker is installed
-if ! command -v docker &> /dev/null
-then
+if ! command_exists docker; then
     echo "Docker is not installed. Please install Docker first."
-    exit
-fi
-
-# Check if Docker Compose is installed
-if ! command -v docker compose &> /dev/null
-then
-    echo "Docker Compose is not installed. Please install Docker Compose."
-    exit
-fi
-
-# Build the custom images
-echo "Building custom Jenkins image..."
-docker build -t jenkins-server ./jenkins
-if [ $? -ne 0 ]; then
-    echo "Failed to build Jenkins image. Exiting."
     exit 1
 fi
 
-# Start the application
-docker compose up --build
+# Check if Docker Compose is installed
+if ! command_exists docker compose; then
+    echo "Docker Compose is not installed. Please install Docker Compose."
+    exit 1
+fi
 
-echo "Application is running. You can access it at http://localhost:8080"
+# Checks if a Docker image has already been built.
+# If not, build it.
+build_image_if_needed() {
+    local image_name="$1"
+    local dockerfile_dir="$2"
+    
+    if [[ "$(docker images -q $image_name 2> /dev/null)" == "" ]]; then
+        echo "Building custom image: $image_name..."
+        docker build -t "$image_name" "$dockerfile_dir"
+        if [ $? -ne 0 ]; then
+            echo "Failed to build $image_name image. Exiting."
+            exit 1
+        fi
+    else
+        echo "Image $image_name already exists. Skipping build."
+    fi
+}
+
+# Build the custom Jenkins image if necessary
+build_image_if_needed jenkins-server ./jenkins
+
+# Start the application
+docker compose up
+if [ $? -ne 0 ]; then
+    echo "Failed to start Docker Compose. Exiting."
+    exit 1
+fi
